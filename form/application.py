f19c2a2371853ce16d72c806043e4bdde5a0c09f
flechita-veloz <79392249+flechita-veloz@users.noreply.github.com>
2022-05-02 03:39:21 -0500

    Add files via upload

--- form/application.py
+++ form/application.py
@@ -7,27 +7,31 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/<string:name>")
-def session(name):
-	return render_template(
-		"session.html",
-		name=name,
-		)
-
 """el request es de tipo POST"""
 @app.route("/hello", methods=["POST"]) 
 def login():
-	name = request.form.get("nombre")
+	"""busca de nuestro form el atributo name es cual es igual a pais"""
+	pais = request.form.get("pais")  #Var
+	nombres = request.form.get("nombres") 
+	apellidos = request.form.get("apellidos") 
+	direccion = request.form.get("direccion") 
+	ciudad = request.form.get("ciudad")
+	provincia = request.form.get("provincia") 
+	codigo_postal = request.form.get("codigo_postal")
+	telefono = request.form.get("telefono")
+	email = request.form.get("email") 
+	password = request.form.get("password")
+
 	return render_template(
 		"session.html",
-		name=name,
+		pais=pais, #establece la variable pais de session y lo iguala a var
+		nombres=nombres,
+		apellidos=apellidos,
+		direccion=direccion,
+		ciudad=ciudad,
+		provincia=provincia,
+		telefono= telefono,
+		codigo_postal=codigo_postal,
+		email=email,
+		password=password,
 		)
-
-@app.route("/users")
-def names():
-	# Query a DB for users.
-	name_list = ["Jose", "Pedro", "Maria", "Luis"]
-	return render_template(
-		"list.html",
-		nombres=name_list,
-	)

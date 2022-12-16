c1e6072e7f6ca6a5efcaa78811fb95a1a746ce30
flechita-veloz <79392249+flechita-veloz@users.noreply.github.com>
2022-12-15 19:01:28 -0500

    Add files via upload

---
+++ Xamarin - Formulario y CV/FormCV/FormCV/FormCV/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormCV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Formulario();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
